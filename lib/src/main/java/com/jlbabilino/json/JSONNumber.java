1209b9b6fd98feda72d0fb630700a64126cbabe9
Justin Babilino <jlbabilino@gmail.com>
2021-11-14 16:33:03 -0500

    Modifications to build.gradle

--- lib/src/main/java/com/jlbabilino/json/JSONNumber.java
+++ lib/src/main/java/com/jlbabilino/json/JSONNumber.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Triple Helix Robotics - FRC Team 2363
+ * Copyright (C) 2021 Justin Babilino
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
